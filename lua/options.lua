041784c64b45da297e2903aaa7e7aca2baf43c53
Fer L <afriguez@gmail.com>
2023-03-08 12:21:19 -0500

    feat: add nvim-tree startup functionality

--- lua/options.lua
+++ lua/options.lua
@@ -22,3 +22,16 @@ vim.g.mkdp_refresh_slow = 1
 vim.g.vim_jsx_pretty_colorful_config = 1
 
 vim.notify = require('notify')
+
+local function open_nvim_tree(data)
+  local directory = vim.fn.isdirectory(data.file) == 1
+
+  if not directory then
+    return
+  end
+
+  vim.cmd.cd(data.file)
+  require("nvim-tree.api").tree.open()
+end
+
+vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
