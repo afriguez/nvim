bbaef8159ca4bc34f2e9965a947c43c4f1c1b7b0
Fer L <afriguez@gmail.com>
2023-05-08 20:30:21 -0500

    feat: make all keymaps silent

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,23 +1,23 @@
 -- telescope
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>gi', builtin.git_files, {})
-vim.keymap.set('n', '<leader>fi', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+vim.keymap.set('n', '<leader>gi', builtin.git_files, { silent = true })
+vim.keymap.set('n', '<leader>fi', builtin.find_files, { silent = true })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { silent = true })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { silent = true })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { silent = true })
 
 -- vim-fugitive
-vim.keymap.set('n', '<leader>gs', ':Git<CR>')
-vim.keymap.set('n', '<leader>gj', ':diffget //3<CR>')
-vim.keymap.set('n', '<leader>gf', ':diffget //2<CR>')
-vim.keymap.set('n', '<leader>gk', ':Git commit<CR>')
+vim.keymap.set('n', '<leader>gs', ':Git<CR>', { silent = true })
+vim.keymap.set('n', '<leader>gj', ':diffget //3<CR>', { silent = true })
+vim.keymap.set('n', '<leader>gf', ':diffget //2<CR>', { silent = true })
+vim.keymap.set('n', '<leader>gk', ':Git commit<CR>', { silent = true })
 
 -- nvim-tree
-vim.keymap.set('n', '<C-n>', ':NvimTreeToggle<CR>')
+vim.keymap.set('n', '<C-n>', ':NvimTreeToggle<CR>', { silent = true })
 
 -- HopWord
-vim.keymap.set('n', '<leader>hw', ':HopWord<CR>')
+vim.keymap.set('n', '<leader>hw', ':HopWord<CR>', { silent = true })
 
 -- harpoon
-vim.keymap.set('n', '<leader>hf', require('harpoon.mark').add_file, {})
-vim.keymap.set('n', '<leader>hl', require('harpoon.ui').toggle_quick_menu, {})
+vim.keymap.set('n', '<leader>hf', require('harpoon.mark').add_file, { silent = true })
+vim.keymap.set('n', '<leader>hl', require('harpoon.ui').toggle_quick_menu, { silent = true })
