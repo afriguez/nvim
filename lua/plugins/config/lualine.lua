86f987944d9356df900039d753e829d50343df08
Fer L <afriguez@gmail.com>
2023-05-09 10:17:50 -0500

    feat: restore lualine

--- lua/plugins/config/lualine.lua
+++ lua/plugins/config/lualine.lua
@@ -2,21 +2,4 @@ local lualine = require('lualine')
 
 local theme = require('lualine.themes.dracula')
 
-lualine.setup {
-	options = {
-		theme = theme
-	},
-	sections = {
-		lualine_x = {
-			{
-				require('noice').api.status.command.get,
-				cond = require('noice').api.status.command.has,
-				color = { fg = "#ff9e64" },
-			},
-			{
-				require('noice').api.status.mode.get,
-				cond = require('noice').api.status.mode.has,
-				color = { fg = "#ff9e64" },
-			},
-		},
-	}, }
+lualine.setup { options = { theme = theme } }
