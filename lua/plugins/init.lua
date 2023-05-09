f22c9a488c21df34fe1de22521fd9de4b06a26fc
Fer L <afriguez@gmail.com>
2023-05-08 20:56:32 -0500

    feat: add noice, gitsigns & indent_blankline plugins
    - feat: remove wilder plugin & configuration file

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,59 +1,66 @@
-local install_path = vim.fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+local install_path = vim.fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
 local repository = 'https://github.com/wbthomason/packer.nvim'
 
 if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-	PACKER_BOOTSTRAP = vim.fn.system({'git', 'clone', '--depth', '1', repository, install_path})
+	PACKER_BOOTSTRAP = vim.fn.system({ 'git', 'clone', '--depth', '1', repository, install_path })
 end
 
 local ok, _ = pcall(require, "packer")
 if not ok then return end
 
 require('plugins.config.nvim_tree')
-require('plugins.config.wilder')
 require('plugins.config.hop')
 require('plugins.config.lualine')
 require('plugins.config.presence')
 require('plugins.config.mason')
 require('plugins.config.cmp')
+require('plugins.config.gitsigns')
+require('plugins.config.indent_blankline')
+require('plugins.config.noice')
 
 return require('packer').startup(function(use)
-  use 'tpope/vim-fugitive'
-  use 'jiangmiao/auto-pairs'
-  use 'mattn/emmet-vim'
-  use 'nvim-lua/plenary.nvim'
-  use 'nvim-telescope/telescope.nvim'
-  use 'ThePrimeagen/harpoon'
-  use 'phaazon/hop.nvim'
-  use 'gelguy/wilder.nvim'
+	use 'tpope/vim-fugitive'
+	use 'lewis6991/gitsigns.nvim'
 
-  use 'elixir-editors/vim-elixir'
-  use 'mhinz/vim-mix-format'
-  use 'yuezk/vim-js'
-  use 'HerringtonDarkholme/yats.vim'
-  use 'MaxMEllon/vim-jsx-pretty'
+	use 'jiangmiao/auto-pairs'
+	use 'mattn/emmet-vim'
+	use 'nvim-lua/plenary.nvim'
+	use 'nvim-telescope/telescope.nvim'
+	use 'ThePrimeagen/harpoon'
+	use 'phaazon/hop.nvim'
+	use 'gelguy/wilder.nvim'
 
-  use 'williamboman/mason.nvim'
-  use 'williamboman/mason-lspconfig.nvim'
-  use 'neovim/nvim-lspconfig'
+	use 'elixir-editors/vim-elixir'
+	use 'mhinz/vim-mix-format'
+	use 'yuezk/vim-js'
+	use 'HerringtonDarkholme/yats.vim'
+	use 'MaxMEllon/vim-jsx-pretty'
 
-  use 'hrsh7th/cmp-nvim-lsp'
-  use 'hrsh7th/cmp-buffer'
-  use 'hrsh7th/nvim-cmp'
-  use 'hrsh7th/cmp-vsnip'
-  use 'hrsh7th/vim-vsnip'
-  use 'hrsh7th/cmp-path'
+	use 'williamboman/mason.nvim'
+	use 'williamboman/mason-lspconfig.nvim'
+	use 'neovim/nvim-lspconfig'
 
-  use 'afriguez/dracula.nvim'
+	use 'hrsh7th/cmp-nvim-lsp'
+	use 'hrsh7th/cmp-buffer'
+	use 'hrsh7th/nvim-cmp'
+	use 'hrsh7th/cmp-vsnip'
+	use 'hrsh7th/vim-vsnip'
+	use 'hrsh7th/cmp-path'
 
-  use {
-	'nvim-lualine/lualine.nvim',
-	requires = { 'kyazdani42/nvim-web-devicons', opt = true }
-  }
-  use 'nvim-tree/nvim-web-devicons'
-  use 'nvim-tree/nvim-tree.lua'
-  use 'rcarriga/nvim-notify'
+	use 'afriguez/dracula.nvim'
 
-  use 'andweeb/presence.nvim'
+	use {
+		'nvim-lualine/lualine.nvim',
+		requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+	}
+	use 'nvim-tree/nvim-web-devicons'
+	use 'nvim-tree/nvim-tree.lua'
+	use 'rcarriga/nvim-notify'
+	use 'lukas-reineke/indent-blankline.nvim'
+	use 'folke/noice.nvim'
+	use 'MunifTanjim/nui.nvim'
 
-  if PACKER_BOOTSTRAP then require('packer').sync() end
+	use 'andweeb/presence.nvim'
+
+	if PACKER_BOOTSTRAP then require('packer').sync() end
 end)
