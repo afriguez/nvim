2edae6f59a531858950a3ca75417aa731d8594c6
Fer L. <afriguez@gmail.com>
2024-01-21 18:02:59 -0500

    feat: update catppuccin config

--- lua/plugins/config/catppuccin.lua
+++ lua/plugins/config/catppuccin.lua
@@ -1,3 +1,16 @@
 require('catppuccin').setup({
-	transparent_background = true
+	transparent_background = true,
+	integrations = {
+		notify = true,
+		harpoon = true,
+		hop = true,
+		mason = true
+	},
+	styles = {
+		comments = { 'italic' },
+		conditionals = { 'italic' },
+		loops = { 'italic' },
+		keywords = { 'italic' },
+		types = { 'italic' }
+	}
 })
