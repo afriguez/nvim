06cdd96a8b28eb7513fe3fa2b81d23ecde8a565f
Fer L. <afriguez@gmail.com>
2024-01-25 22:40:34 -0500

    feat: set auto_install to true

--- lua/plugins/config/treesitter.lua
+++ lua/plugins/config/treesitter.lua
@@ -1,6 +1,6 @@
 require('nvim-treesitter.configs').setup {
 	ensure_installed = { "eex", "heex", "html", "surface", "go" },
-	auto_install = false,
+	auto_install = true,
 	highlight = { enable = true },
 	ignore_install = { "all" }
 }
