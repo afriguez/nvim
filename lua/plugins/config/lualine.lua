00651e8e02108110e864b010a53ba8cf737f7f01
Fer L. <afriguez@gmail.com>
2024-02-02 17:23:57 -0500

    feat: set catppuccin as lualine theme

--- lua/plugins/config/lualine.lua
+++ lua/plugins/config/lualine.lua
@@ -1,5 +1,3 @@
-local lualine = require('lualine')
-
-local theme = require('lualine.themes.dracula')
-
-lualine.setup { options = { theme = theme } }
+require('lualine').setup({
+	options = { theme = "catppuccin" }
+})
