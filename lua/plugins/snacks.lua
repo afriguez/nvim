62a6b520a476f734fb20f56a159f870496986ce9
Fer L. <afriguez@gmail.com>
2025-11-19 07:43:58 -0500

    feat: add zen snack

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -4,7 +4,22 @@ return {
 		priority = 1000,
 		lazy = false,
 		opts = {
-			notifier = { enabled = true, timeout = 3000 }
-		}
+			notifier = { enabled = true, timeout = 3000 },
+			zen = {
+				enabled = true,
+				toggles = {
+					dim = true,
+					git_signs = true,
+					diagnostics = true,
+				}
+			}
+		},
+		keys = function()
+			local snacks = require("snacks")
+			return {
+				{ "<leader>z", function() snacks.zen() end },
+				{ "<leader>Z", function() snacks.zen.zoom() end }
+			}
+		end,
 	}
 }
