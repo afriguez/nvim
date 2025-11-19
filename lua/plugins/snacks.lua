475bf9ebf73a03a75da7b559135a8e8430d6a049
Fer L. <afriguez@gmail.com>
2025-11-19 11:39:37 -0500

    feat: remove zen from snacks

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -5,21 +5,6 @@ return {
 		lazy = false,
 		opts = {
 			notifier = { enabled = true, timeout = 3000 },
-			zen = {
-				enabled = true,
-				toggles = {
-					dim = true,
-					git_signs = true,
-					diagnostics = true,
-				}
-			}
-		},
-		keys = function()
-			local snacks = require("snacks")
-			return {
-				{ "<leader>z", function() snacks.zen() end },
-				{ "<leader>Z", function() snacks.zen.zoom() end }
-			}
-		end,
+		}
 	}
 }
