2ce542accaebfd5c9292388892b454b3098a9765
Fer L. <afriguez@gmail.com>
2025-11-21 11:49:09 -0500

    feat: add keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -31,6 +31,9 @@ vim.keymap.set('n', 'J', ':m .+1<CR>==', opts)
 vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv", opts)
 vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv", opts)
 
+vim.keymap.set('n', '<C-u>', '<C-u>zz', opts)
+vim.keymap.set('n', '<C-d>', '<C-d>zz', opts)
+
 if not (vim.g.hostname == "marija") then
   vim.api.nvim_set_keymap('x', '<leader>y', '"+y', { noremap = true, silent = true })
   vim.api.nvim_set_keymap('n', '<leader>Y', '"+yg_', { noremap = true, silent = true })
