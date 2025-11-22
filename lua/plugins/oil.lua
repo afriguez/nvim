bbdf4bee376258f7845e5b14fe880b8428fded58
Juan A. <adrephos@gmail.com>
2025-11-22 14:44:27 -0500

    feat: update oil configuration

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,3 +1,28 @@
+function _G.get_oil_winbar()
+  local bufnr = vim.api.nvim_win_get_buf(vim.g.statusline_winid)
+  local dir = require("oil").get_current_dir(bufnr)
+  if dir then
+    return vim.fn.fnamemodify(dir, ":~")
+  else
+    -- If there is no current directory (e.g. over ssh), just show the buffer name
+    return vim.api.nvim_buf_get_name(0)
+  end
+end
+
+local copy_entry_path = {
+  desc = "Copy the filepath of the entry under the cursor system to clipboard",
+  callback = function()
+    local oil = require("oil")
+    local entry = oil.get_cursor_entry()
+    local dir = oil.get_current_dir()
+    if not entry or not dir then
+      return
+    end
+    vim.fn.setreg("+", dir .. entry.name)
+    vim.notify(dir .. entry.name .. " copied to clipboard", vim.log.levels.INFO)
+  end,
+}
+
 return {
   {
     "stevearc/oil.nvim",
@@ -5,7 +30,16 @@ return {
     opts = {
       columns = { "icon" },
       win_options = {
-        signcolumn = "yes:2",
+        winbar = "%!v:lua.get_oil_winbar()",
+      },
+      view_options = {
+        show_hidden = true,
+        is_always_hidden = function(name, _)
+          return name == '..'
+        end,
+      },
+      keymaps = {
+        ["cc"] = { copy_entry_path, mode = "n" },
       },
     },
     lazy = false,
@@ -16,10 +50,7 @@ return {
     end,
   },
   {
-    "refractalize/oil-git-status.nvim",
-    dependencies = {
-      "stevearc/oil.nvim",
-    },
-    config = true,
-  },
+    "benomahony/oil-git.nvim",
+    dependencies = { "stevearc/oil.nvim" },
+  }
 }
