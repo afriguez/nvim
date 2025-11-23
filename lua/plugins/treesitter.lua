afc12fb86701bf1b47ce3d2f1a52ad350f1db33e
Juan A. <adrephos@gmail.com>
2025-11-23 16:36:03 -0500

    feat: change config entry to init

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,26 +1,29 @@
 return {
-	{
-		"nvim-treesitter/nvim-treesitter",
-		dependencies = { "nvim-treesitter/nvim-treesitter-context" },
-		branch = "main",
-		lazy = false,
-		build = ":TSUpdate",
-		opts = {
-			ensured_installed = {
-				"go",
-			},
-			highlight = {
-				enabled = true
-			}
-		},
-		config = function()
-            vim.api.nvim_create_autocmd('FileType', {
-                group = vim.api.nvim_create_augroup('Highlighter', {}),
-                pattern = 'markdown',
-                callback = function(args)
-                    vim.treesitter.start(args.buf)
-                end,
-            })
+  {
+    "nvim-treesitter/nvim-treesitter",
+    dependencies = { "nvim-treesitter/nvim-treesitter-context" },
+    branch = "main",
+    lazy = false,
+    build = ":TSUpdate",
+    opts = {
+      ensured_installed = {
+        "go",
+        "latex",
+        "gitcommit"
+      },
+      auto_install = true,
+      highlight = {
+        enabled = true
+      },
+    },
+    init = function()
+      vim.api.nvim_create_autocmd('FileType', {
+        group = vim.api.nvim_create_augroup('Highlighter', {}),
+        pattern = 'markdown',
+        callback = function(args)
+          vim.treesitter.start(args.buf)
         end,
-	}
+      })
+    end,
+  }
 }
