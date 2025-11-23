c4d0e8549010f8798d02702c8509929c698c0a75
Juan A. <adrephos@gmail.com>
2025-11-23 16:47:28 -0500

    feat: change cmdline popup width

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -11,7 +11,7 @@ return {
             col = "50%",
           },
           size = {
-            width = "75%",
+            width = "30%",
             height = "auto",
           },
         },
@@ -22,7 +22,7 @@ return {
             col = "50%",
           },
           size = {
-            width = "75%",
+            width = "30%",
             height = 10,
           },
           border = {
