b394074c7b1c86705d251b53860da998c8741cd3
Fer L. <afriguez@gmail.com>
2025-11-24 14:19:27 -0500

    feat: update treesitter config

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,26 +2,14 @@ return {
   {
     "nvim-treesitter/nvim-treesitter",
     dependencies = { "nvim-treesitter/nvim-treesitter-context" },
-    branch = "main",
+    branch = "master",
     lazy = false,
     build = ":TSUpdate",
-    init = function()
-      local TS = require("nvim-treesitter")
-      TS.install(
-        { "go", "latext", "gitcommit" }
-      )
-
-      local langs = TS.get_installed()
-      vim.notify(vim.inspect(langs), vim.log.levels.INFO)
-
-      vim.api.nvim_create_autocmd("FileType", {
-        pattern = langs,
-        callback = function()
-          vim.treesitter.start()
-          vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
-          vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-        end,
-      })
-    end,
+    opts = {
+      auto_install = true,
+      highlight = { enable = true },
+      ignore_install = { "all" }
+    },
+    main = "nvim-treesitter.configs"
   }
 }
