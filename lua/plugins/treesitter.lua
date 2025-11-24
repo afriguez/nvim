cf8967cf17401bf073017fa5c2da7e610e203ee7
Juan A. <adrephos@gmail.com>
2025-11-24 10:40:51 -0500

    feat: use autocmd to load treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,23 +5,14 @@ return {
     branch = "main",
     lazy = false,
     build = ":TSUpdate",
-    opts = {
-      ensured_installed = {
-        "go",
-        "latex",
-        "gitcommit"
-      },
-      auto_install = true,
-      highlight = {
-        enabled = true
-      },
-    },
     init = function()
-      vim.api.nvim_create_autocmd('FileType', {
-        group = vim.api.nvim_create_augroup('Highlighter', {}),
-        pattern = 'markdown',
-        callback = function(args)
-          vim.treesitter.start(args.buf)
+      require('nvim-treesitter').install(
+        { "go", "latext", "gitcommit" }
+      ):wait(300000)
+
+      vim.api.nvim_create_autocmd("BufReadPost", {
+        callback = function()
+          vim.treesitter.start()
         end,
       })
     end,
