f09cda6cab6f0ed77b1cbda0dcbbea8ce25c46ff
Juan A. <adrephos@gmail.com>
2026-08-18 13:49:51 -0500

    feat(treesitter): migrate to nvim-treesitter main branch API

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -11,14 +11,23 @@ return {
         }
       }
     },
-    branch = "master",
+    branch = "main",
     lazy = false,
     build = ":TSUpdate",
     opts = {
       auto_install = true,
-      highlight = { enable = true },
-      ignore_install = { "all" }
+      ignore_install = { "all", "mlir" }
     },
-    main = "nvim-treesitter.configs"
+    init = function ()
+      vim.api.nvim_create_autocmd('FileType', {
+        callback = function ()
+          -- Enable treesitter highlighting and disable regex syntax
+          pcall(vim.treesitter.start)
+          -- Enable treesitter-based indentation
+          vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+        end
+      })
+    end,
+    main = "nvim-treesitter"
   }
 }
